25efef0edd442c3f2439bf4c5dd43516bdcb2a88
Pedro H. Marques <opedrohenriquemarques@gmail.com>
2024-02-18 20:39:42 -0300

    Logo - change to simple logo and change tab logo

--- src/components/Header/components/Logo.jsx
+++ src/components/Header/components/Logo.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import logoPh from "../../../assets/ph-logo.png";
 
 const Logo = () => {
   return (
     <div className="relative ml-10 flex h-12 w-12 items-center justify-center border-2 border-green hover:text-green">
-      PH
+      <a href="#home">
+        <img src={logoPh} />
+      </a>
     </div>
   );
 };
