b4ec62dbbeff04ecd45b18713682747d1679fdc0
Pedro H. Marques <opedrohenriquemarques@gmail.com>
2024-02-20 16:39:12 -0300

    Add home animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,39 @@
 import React from "react";
+import { animated } from "react-spring";
+import { useHomeAnimation } from "../utils/animation";
 
 const Home = () => {
+  const { spanAnimation, h1Animation, h2Animation, pAnimation } =
+    useHomeAnimation();
+
   return (
     <section
       id="home"
       className="flex h-screen w-full flex-col items-start justify-start md:-mb-44"
     >
-      <span className="font-mono text-green">Oi, eu sou o </span>
-      <h1 className="text-3xl font-bold md:text-6xl">Pedro H. Marques.</h1>
-      <h2 className="text-3xl font-bold text-light-slate md:text-6xl">
+      <animated.span className="font-mono text-green" style={spanAnimation}>
+        Oi, eu sou o{" "}
+      </animated.span>
+      <animated.h1
+        className="text-3xl font-bold md:text-6xl"
+        style={h1Animation}
+      >
+        Pedro H. Marques.
+      </animated.h1>
+      <animated.h2
+        className="text-3xl font-bold text-light-slate md:text-6xl"
+        style={h2Animation}
+      >
         Eu construo coisas para a web.
-      </h2>
-      <p className="mt-10 w-2/3 text-start text-lg text-light-slate">
-        Eu sou um desenvolver web com mais de três anos de experiência. Focado
-        em entregar maravilhosas experienciais digitais, atualmente estou focado
+      </animated.h2>
+      <animated.p
+        className="mt-10 w-2/3 text-start text-lg text-light-slate"
+        style={pAnimation}
+      >
+        Eu sou um desenvolvedor web com mais de três anos de experiência. Focado
+        em entregar maravilhosas experiências digitais, atualmente estou focado
         no frontend e suas maravilhas!
-      </p>
+      </animated.p>
     </section>
   );
 };
---
+++ src/utils/animation.js
@@ -0,0 +1,33 @@
+import { useSpring } from "react-spring";
+
+export const useHomeAnimation = () => {
+  const spanAnimation = useSpring({
+    from: { opacity: 0, transform: "translateY(10px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    delay: 100,
+    config: { duration: 500 },
+  });
+
+  const h1Animation = useSpring({
+    from: { opacity: 0, transform: "translateY(10px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    delay: 200,
+    config: { duration: 500 },
+  });
+
+  const h2Animation = useSpring({
+    from: { opacity: 0, transform: "translateY(10px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    delay: 300,
+    config: { duration: 500 },
+  });
+
+  const pAnimation = useSpring({
+    from: { opacity: 0, transform: "translateY(10px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    delay: 400,
+    config: { duration: 500 },
+  });
+
+  return { spanAnimation, h1Animation, h2Animation, pAnimation };
+};
